aa7c3dd585b2ddb18a3a7cf7be18479e6d75ac81
Sean Villanoy <seanvillanoy@Seans-MacBook-Pro.local>
2020-11-05 18:41:26 -0500

    Practiced for loops over dictionaries & f strings

--- Python_practice.py
+++ Python_practice.py
@@ -1,1 +1,10 @@
-print("Hello World")
+item = 'coat'
+sticker_price= 1000
+tax_rate= 0.13
+print(f'Your {item} will cost ${(sticker_price * (1 + tax_rate)):,.2f} after taxes.')
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for key, value in counties_dict.items():
+    print(f'{key} county has {value:,} registered voters.')
+
+
